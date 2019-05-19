511f6788c54fb3d214258c38a48dba7d793f5c47
Rene B. Dena <renedena842@gmail.com>
2019-05-19 12:10:31 -0700

    Add files via upload

---
+++ Homework II/ServerScript.py
@@ -0,0 +1,23 @@
+## Homework No III - V 1.0.0 - Server Script
+## Author: Dena, Rene
+## Last Modified: 3/19/19
+
+#________________________________________________________Script_________________________________________________________
+
+#!/usr/bin/python           # This is server.py file
+
+print("\n\n")
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print('Got connection from', addr)
+   print("\n")
+   c.send(b'Thank you for connecting')
+   c.close()                # Close the connection
